f296ee057904a3bdc622718bc9af07dfcd5c8ca9
Bryan Mills <bryan@millz.net>
2010-07-25 17:48:38 -0400

    first commit

---
+++ jquery.writelimit.js
@@ -0,0 +1,115 @@
+/* Copyright (c) 2010 Bryan Mills
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
+(function($){
+   var WriteLimit = function(element, options)
+   {
+       var elem = $(element);
+       var obj = this;
+       var settings = $.extend({
+           max: 140,
+           unit: 'chars',
+           counter: null,
+           countDirection: 'down',
+           overClass: 'over-limit',
+           uncounted: []
+       }, options || {});
+       
+       // Public methods             
+       this.refresh = function()
+       {
+          var counter = settings['counter'];
+          var max = settings['max'];
+          var input = getInput();
+          var counted = doCount(input);
+          
+          if (settings['countDirection'] == 'down') {
+              var newValue = max - counted;
+          } else {
+              var newValue = counted;
+          }
+          
+          if (counted > max) {
+              counter.addClass(settings['overClass']);
+          } else {
+              counter.removeClass(settings['overClass']);
+          }
+          
+          counter.text(newValue);
+    
+       };
+
+       // Private methods
+       var getInput = function()
+       {
+          return elem.val();
+       };       
+       
+       var doCount = function(input)
+       {
+           for (var character in settings['uncounted']) {
+               input = input.replace(character, '');
+           }
+           
+           if (settings['unit'] == 'chars') { 
+               return input.length
+           }
+           
+           if (settings['unit'] == 'words') {
+               // convert newlines to spaces
+               input = input.replace(/\s/g, ' ');
+               input = input.split(' ');
+               
+               // get rid of any element that contain nothing
+               for(var i=0; i<input.length; i++) {
+                   if (!input[i].length > 0) {
+                       input.splice(i,1);
+                   }
+               }
+               
+               return input.length;
+           }
+       };
+   };
+
+   $.fn.writeLimit = function(options)
+   {
+       return this.each(function()
+       {
+           var element = $(this);
+          
+           // Return early if this element already has a plugin instance
+           if (element.data('writeLimit')) return;
+
+           // pass options to plugin constructor
+           var writeLimit = new WriteLimit(this, options);
+
+           // Store plugin object in this element's data
+           element.data('writeLimit', writeLimit);
+
+           writeLimit.refresh();
+           element.keyup(function() {
+               writeLimit.refresh();
+           });
+           
+           return this;
+       });
+   };
+})(jQuery);
